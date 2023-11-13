4ccd619a29632941d80f9f4e240bf0d8b07a4ffc
dalepotter <dalepotter@gmail.com>
2023-11-13 13:03:30 +0000

    Initial commit of auto-generated code
    Code generated using `wagtail start mysite`.

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-^_)9&p9iuj1j*0w8#wr%aropn%(uz-c2l)ljc@w4f@*12jxrh!"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
