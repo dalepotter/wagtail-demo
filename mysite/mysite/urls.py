e7c8a0089e4a93c54655e5922b0b29326dc65392
dalepotter <dalepotter@gmail.com>
2023-12-14 12:10:33 +0000

    Use desktop as default preview mode

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,12 +3,62 @@ from django.urls import include, path
 from django.contrib import admin
 
 from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.admin.views.pages.create import CreateView
+from wagtail.admin.views.pages.edit import EditView
+from wagtail.admin.ui.components import MediaContainer
+from wagtail.admin.ui.side_panels import PreviewSidePanel
 from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 from search import views as search_views
 
+
+class CustomPreviewSidePanel(PreviewSidePanel):
+    class SidePanelToggle(PreviewSidePanel.SidePanelToggle):
+        icon_name = "desktop"
+
+    template_name = "custom-preview.html"
+
+
+class CustomCreateView(CreateView):
+    def get_side_panels(self):
+        # Replace any PreviewSidePanel in parent method with CustomPreviewSidePanel
+        media_container = MediaContainer()
+        for side_panel in super().get_side_panels():
+            if isinstance(side_panel, PreviewSidePanel):
+                side_panel = CustomPreviewSidePanel(
+                    self.page, self.request, preview_url=self.get_preview_url()
+                )
+
+            media_container.append(side_panel)
+
+        return media_container
+
+
+class CustomEditView(EditView):
+    def get_side_panels(self):
+        # Replace any PreviewSidePanel in parent method with CustomPreviewSidePanel
+        media_container = MediaContainer()
+        for side_panel in super().get_side_panels():
+            if isinstance(side_panel, PreviewSidePanel):
+                side_panel = CustomPreviewSidePanel(
+                    self.page, self.request, preview_url=self.get_preview_url()
+                )
+
+            media_container.append(side_panel)
+
+        return media_container
+
+
 urlpatterns = [
+    # Override create view in order to apply custom view
+    path(
+        "admin/pages/add/<slug:content_type_app_name>/<slug:content_type_model_name>/<int:parent_page_id>/",
+        CustomCreateView.as_view(),
+        name="add",
+    ),
+    # Override edit view in order to apply custom view
+    path("admin/pages/<int:page_id>/edit/", CustomEditView.as_view(), name="edit"),
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
